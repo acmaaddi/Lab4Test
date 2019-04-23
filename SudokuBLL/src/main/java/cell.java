48acbbf8198782dc9ebf96570f037bdfefe0adae
amaad <amaad@128.4.138.13>
2019-04-22 23:54:14 -0400

    commit4

---
+++ SudokuBLL/src/main/java/cell.java
@@ -0,0 +1,15 @@
+import java.util.Collections;
+import java.util.List;
+
+public class cell {
+
+	
+	
+	
+	public void  ShuffleValidValues(List<?> listOfValues){
+		Collections.shuffle(listOfValues); 
+	}
+	
+	
+	
+}
